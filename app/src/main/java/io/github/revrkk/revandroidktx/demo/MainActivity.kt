e74caaebdf43f15a27834422635d61ffbc0ef487
RevanthKrishnaKumar V <rev.rkk@gmail.com>
2019-10-26 20:16:47 +0300

    - Changed the app package to demo

--- app/src/main/java/io/github/revrkk/revandroidktx/demo/MainActivity.kt
+++ app/src/main/java/io/github/revrkk/revandroidktx/demo/MainActivity.kt
@@ -1,12 +1,14 @@
-package io.github.revrkk.revandroidktx
+package io.github.revrkk.revandroidktx.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.revrkk.revandroidktx.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showLongToast("Hello")
     }
 }
